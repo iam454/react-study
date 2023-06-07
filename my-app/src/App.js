aea9758b4efde6d5f01acea1afbada773e476a4f
iam454 <realseogy@gmail.com>
2023-06-08 08:22:15 +0900

    feat: 2-6.LifeCycle
    컴포넌트의 생명주기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,16 @@ import Composition from "./components/2-4.Props/Composition";
 import Extraction from "./components/2-4.Props/Extraction/Extraction";
 import ClassComponent from "./components/2-5.State/ClassComponent";
 import FunctionalComponent from "./components/2-5.State/FunctionalComponent";
+import ClassComponent2 from "./components/2-6.LifeCycle/ClassComponent";
 
 function App() {
   return (
     <div className="App">
-      <Composition />
+      {/* <Composition />
       <Extraction />
       <ClassComponent />
-      <FunctionalComponent />
+      <FunctionalComponent /> */}
+      <ClassComponent2 />
     </div>
   );
 }
---
+++ my-app/src/components/2-6.LifeCycle/ClassComponent.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export default class ClassComponent extends Component {
+  constructor(props) {
+    super();
+    console.log("constructor");
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.timerId = setInterval(() => this.tick(), 10000);
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    console.log("tick");
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>Hello</h1>
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
