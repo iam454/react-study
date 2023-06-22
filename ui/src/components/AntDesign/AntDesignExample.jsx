4265ca4cef38f2aa4f572d92f627e3a8f62d3039
iam454 <realseogy@gmail.com>
2023-06-22 11:10:10 +0900

    feat: Ant Design 다루기
    Layout
    Form
    Table
    Modal

--- ui/src/components/AntDesign/AntDesignExample.jsx
+++ ui/src/components/AntDesign/AntDesignExample.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import GetStart from "./GetStart";
-import IconExample from "./IconExample";
-import TypographyExample from "./TypographyExample";
+// import GetStart from "./GetStart";
+// import IconExample from "./IconExample";
+// import TypographyExample from "./TypographyExample";
+
+import LayoutExample from "./LayoutExample";
+import FormExample from "./FormExample";
+import TableExample from "./TableExample";
+import ModalExample from "./ModalExample";
 
 export default function AntDesignExample() {
   return (
     <div>
-      <GetStart />
-      <IconExample />
-      <TypographyExample />
+      {/* <GetStart /> */}
+      {/* <IconExample /> */}
+      {/* <TypographyExample /> */}
+
+      <LayoutExample />
+      <FormExample />
+      <TableExample />
+      <ModalExample />
     </div>
   );
 }
