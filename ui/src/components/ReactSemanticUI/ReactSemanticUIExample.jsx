f2a08885312181508f30e9f00009096f6b0c77dc
iam454 <realseogy@gmail.com>
2023-06-22 15:03:00 +0900

    feat: React Semantic UI 다루기
    ❌ Visibility + Progress
    codesandbox에서는 동작하는데 무슨 이유일까..
    create-react-app에서 semantic ui 쓰는법이 달라서 그런가?(추측)

--- ui/src/components/ReactSemanticUI/ReactSemanticUIExample.jsx
+++ ui/src/components/ReactSemanticUI/ReactSemanticUIExample.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import ButtonExampleLabeledBasic from "./ButtonExampleLabeledBasic";
-import ListExample from "./ListExample";
+// import ButtonExampleLabeledBasic from "./ButtonExampleLabeledBasic";
+// import ListExample from "./ListExample";
+import VisibilityProgressExample from "./VisibilityProgressExample";
 
 export default function ReactSemanticUIExample() {
   return (
     <>
-      <ButtonExampleLabeledBasic />
-      <ListExample />
+      {/* <ButtonExampleLabeledBasic /> */}
+      {/* <ListExample /> */}
+      <VisibilityProgressExample />
     </>
   );
 }
