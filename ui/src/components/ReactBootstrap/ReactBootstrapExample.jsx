5a8b760bc3043e54081a8bab851968c420b373bd
iam454 <realseogy@gmail.com>
2023-06-23 09:28:55 +0900

    feat: React Bootstrap 다루기
    Carousel
    Modal

--- ui/src/components/ReactBootstrap/ReactBootstrapExample.jsx
+++ ui/src/components/ReactBootstrap/ReactBootstrapExample.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import AlertDismissible from "./AlertDismissible";
-import GridExample from "./GridExample";
-import ButtonExample from "./ButtonExample";
+import CarouselExample from "./CarouselExample";
+import MydModalWithGrid from "./MydModalWithGrid";
+// import AlertDismissible from "./AlertDismissible";
+// import GridExample from "./GridExample";
+// import ButtonExample from "./ButtonExample";
 
 export default function ReactBootstrapExample() {
   return (
     <div>
-      <AlertDismissible />
-      <GridExample />
-      <ButtonExample />
+      {/* <AlertDismissible /> */}
+      {/* <GridExample /> */}
+      {/* <ButtonExample /> */}
+
+      <CarouselExample />
+      <MydModalWithGrid />
     </div>
   );
 }
