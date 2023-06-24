d359f2127948843e3f6d21cc7d4f10ed9061bbbc
iam454 <realseogy@gmail.com>
2023-06-24 16:52:23 +0900

    feat: React Spring 다루기

---
+++ animate/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReactSpringExample from "./components/ReactSpring/ReactSpringExample";
+
+function App() {
+  return (
+    <div className="App">
+      <ReactSpringExample />
+    </div>
+  );
+}
+
+export default App;
