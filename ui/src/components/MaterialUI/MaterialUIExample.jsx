94ba4ec5a45171801ead62e537beb78c6c41ab2b
iam454 <realseogy@gmail.com>
2023-06-24 14:58:56 +0900

    feat: Material UI 다루기
    Buttons with Icon, Label
    Drawer
    Virtualized List

--- ui/src/components/MaterialUI/MaterialUIExample.jsx
+++ ui/src/components/MaterialUI/MaterialUIExample.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import ButtonExample from "./ButtonExample";
-import BasicGrid from "./BasicGrid";
+// import ButtonExample from "./ButtonExample";
+// import BasicGrid from "./BasicGrid";
+import IconLabelButtons from "./IconLabelButtons";
+import TemporaryDrawer from "./TemporaryDrawer";
+import VirtualizedList from "./VirtualizedList";
 
 export default function MaterialUIExample() {
   return (
     <div>
-      <ButtonExample />
-      <BasicGrid />
+      {/* <ButtonExample /> */}
+      {/* <BasicGrid /> */}
+
+      <IconLabelButtons />
+      <TemporaryDrawer />
+      <VirtualizedList />
     </div>
   );
 }
